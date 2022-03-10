abb58c2a2002ae9dae9ad73f8dce54b256c328c5
owenlessel <owenlessel@gmail.com>
2022-03-10 09:58:59 -0600

    done with 18

---
+++ src/Cat.java
@@ -0,0 +1,13 @@
+public class Cat extends Mammals {
+    protected String name;
+    protected String breed;
+
+    public Cat(){
+        super();
+        this.name="Carl";
+        this.breed="Tabby";
+    }
+
+
+
+}
