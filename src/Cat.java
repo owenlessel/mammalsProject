8b5987c2a1f9f1660a5a4193485d95822b0db609
owenlessel <owenlessel@gmail.com>
2022-03-14 08:43:26 -0500

    done :)

--- src/Cat.java
+++ src/Cat.java
@@ -8,6 +8,29 @@ public class Cat extends Mammals {
         this.breed="Tabby";
     }
 
+    public Cat(String name, String breed, boolean hasBackbone, boolean producesMilk, String hairColor,
+    double bodyTempF,
+    int numLegs,
+    boolean liveBirth){
+        this.name = name;
+        this.breed = breed;
+        this.hasBackbone = hasBackbone;
+        this.producesMilk = producesMilk;
+        this.hairColor = hairColor;
+        this.bodyTempF = bodyTempF;
+        this.numLegs = numLegs;
+        this.liveBirth = liveBirth;
+    }
 
+    public String toString() {
+        String report = "";
+        report = this.hasBackbone + " " + this.producesMilk + " " + this.hairColor + " " + this.bodyTempF + " "
+                + numLegs + " " + liveBirth + " " + this.name + " " + this.breed;
 
+        return report;
+    }
+    public void talk(){
+        String sounds="Meow" + " " + "Hiss" + " " + "Purr";
+        System.out.println(sounds);
+    }
 }
